5908c874d20c9f45e735255f66488ffdee10fe87
RG <rg703612@gmail.com>
2020-12-23 14:38:58 +0530

    Dairy Industry Ready to launch.

--- app/src/main/java/com/example/dairy/WalletActivity.java
+++ app/src/main/java/com/example/dairy/WalletActivity.java
@@ -14,11 +14,17 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.razorpay.Checkout;
+import com.razorpay.PaymentData;
+import com.razorpay.PaymentResultWithDataListener;
+
+import org.json.JSONObject;
+
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-public class WalletActivity extends AppCompatActivity {
+public class WalletActivity extends AppCompatActivity implements PaymentResultWithDataListener {
 
     private Button button;
     private EditText editText;
@@ -28,6 +34,8 @@ public class WalletActivity extends AppCompatActivity {
     private Context context = this;
 
     private static final String TAG = "WalletActivity";
+
+    private int money = 0;
 
 
     @Override
@@ -43,6 +51,9 @@ public class WalletActivity extends AppCompatActivity {
         preferences = context.getSharedPreferences("login", Context.MODE_PRIVATE);
         UserId = preferences.getString("user_id", null);
         init();
+
+        Checkout.preload(context);
+
     }
 
     private void init() {
@@ -58,16 +69,84 @@ public class WalletActivity extends AppCompatActivity {
             public void onClick(View v) {
 
                 String amount = editText.getText().toString().trim();
+                money = Integer.parseInt(amount);
 
-                if (Integer.parseInt(amount) <= 0) {
+                if (money <= 0) {
                     Toast.makeText(WalletActivity.this, "Invalid Amount", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                addMoney(amount);
+
+                startPayment();
+
             }
         });
     }
+
+
+    /*------------------------------ Start Payment --------------------------------*/
+
+    public void startPayment() {
+
+        Checkout checkout = new Checkout();
+        checkout.setKeyID("rzp_test_OYPofJwjN32SPE");
+
+        try {
+            JSONObject options = new JSONObject();
+            options.put("name", "Test");
+            options.put("description", "Subscription");
+            options.put("currency", "INR");
+            options.put("amount", String.valueOf(money * 100));
+
+            JSONObject preFill = new JSONObject();
+
+            options.put("prefill", preFill);
+
+
+            checkout.open(this, options);
+        } catch (Exception e) {
+            Toast.makeText(this, "Error in payment: " + e.getMessage(), Toast.LENGTH_SHORT)
+                    .show();
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void onPaymentSuccess(String s, PaymentData paymentData) {
+        try {
+            Log.d(TAG, "onPaymentSuccess: O _ Id " + paymentData.getData());
+            Log.d(TAG, "onPaymentSuccess: P _ Id " + paymentData.getPaymentId());
+            Log.d(TAG, "onPaymentSuccess: Signature" + paymentData.getSignature());
+
+            addMoney(String.valueOf(money));
+
+
+        } catch (Exception e) {
+            Log.d(TAG, "Exception in onPaymentSuccess", e);
+        }
+
+    }
+
+    @Override
+    public void onPaymentError(int i, String s, PaymentData paymentData) {
+        try {
+            Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+
+            Log.d(TAG, "onPaymentFailed: O _ Id" + paymentData.getOrderId());
+            Log.d(TAG, "onPaymentFailed: P _ Id" + paymentData.getPaymentId());
+            Log.d(TAG, "onPaymentFailed: Signature" + paymentData.getSignature());
+
+        } catch (Exception e) {
+            Log.e(TAG, "Exception in onPaymentError", e);
+        }
+
+    }
+
+
+
+    /*------------------------------ Add Money --------------------------------*/
+
 
     private void addMoney(String amount) {
 
